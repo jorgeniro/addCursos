b69661f1e1474ce22134d49df31eb11631e31f7a
jorge niro <niro.jorge@gmail.com>
2022-04-28 11:13:06 -0300

    intentando carrito

--- add.js
+++ add.js
@@ -74,4 +74,30 @@ document.getElementById('curso-list').addEventListener('click', function(e) {
     ui.deleteCurso(e.target);
 })    
 
-    
+// const imagePreview = document.getElementById('img-preview')
+// const imageUploader = document.getElementById('img-uploader')
+
+// imageUploader.addEventListener('change', (e) => {
+//     const file = e.target.files[0];
+// })
+
+const bannerImage = document.getElementById('bannerImg');
+const imgData = getBase64Image(bannerImage);
+localStorage.setItem("imgData", imgData);
+
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
+var dataImage = localStorage.getItem('imgData');
+bannerImg = document.getElementById('tableBanner');
+bannerImg.src = "data:image/png;base64," + dataImage;
